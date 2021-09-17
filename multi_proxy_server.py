cc74091748a5577d78ca19da7d20ba45745464fa
Kevin Zhu <kevinzhu@Kevins-MacBook-Air.local>
2021-09-17 15:30:00 -0600

    MPC and MES implemented

---
+++ multi_proxy_server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import socket
+import time
+from multiprocessing import Process
+
+#define address & buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+#get host information
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbyname( host )
+    except socket.gaierror:
+        print ('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    print (f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+    
+def handle_request():
+
+def main():
+
+if __name__ == "__main__":
+    main()
